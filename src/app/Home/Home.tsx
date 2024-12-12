26a4882802824bcca59b5a517087308e1a4b6871
amirasahraee <87465114+amirasahraee@users.noreply.github.com>
2024-12-12 17:51:15 +0330

    #2 implement Header Home

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -1,27 +1,91 @@
-// react-router-dom
-import { Outlet } from "react-router-dom";
-// components
-import { Modals } from "./_components/Modals/Modals";
-import { Buttons } from "./_components/Buttons/Buttons";
-import { Validation } from "./_components/Validation/Validation";
-import { FormContainer } from "./_components/FormContainer/FormContainer";
+import styles from './Home.module.scss';
+import {Profile, Note} from 'iconsax-react';
+import Notification from '@/assets/svg/notification-bing.svg'
+import Share from '@/assets/svg/share.svg';
 
 export const Home = () => {
-  return (
-    <main
-      style={{
-        gap: "16px",
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <FormContainer />
-      <Modals />
-      <Buttons />
-      <Validation />
-      <Outlet />
-    </main>
-  );
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.header}>
+                <div className={styles.shadow}/>
+                <div className={styles.topHeader}>
+                    <div className={styles.welcome}>
+                        <div className={styles.iconBox}>
+                            <Profile className={styles.icon}/>
+                        </div>
+                        <div className={styles.textBox}>
+                            <div className={styles.text}>کاربر عزیز خوش آمدید!</div>
+                            <div className={styles.date}>اردیبهشت ۱۴۰۳</div>
+                        </div>
+                    </div>
+                    <div className={styles.notificationBox}>
+                        <Notification className={styles.icon}/>
+                        <div className={styles.counter}>۵</div>
+                    </div>
+                </div>
+
+                <div className={styles.week}>
+                    <div className={styles.day}>
+                        <div className={styles.number}>۱۱</div>
+                        <div className={styles.text}>شنبه</div>
+                    </div>
+                    <div className={styles.day}>
+                        <div className={styles.number}>۱۲</div>
+                        <div className={styles.text}>۱شنبه</div>
+                    </div>
+                    <div className={styles.day}>
+                        <div className={styles.number}>۱۳</div>
+                        <div className={styles.text}>۲شنبه</div>
+                    </div>
+                    <div className={styles.daySelect}>
+                        <div className={styles.number}>۱۴</div>
+                        <div className={styles.text}>۳شنبه</div>
+                    </div>
+                    <div className={styles.day}>
+                        <div className={styles.number}>۱۵</div>
+                        <div className={styles.text}>۴شنبه</div>
+                    </div>
+                    <div className={styles.day}>
+                        <div className={styles.number}>۱۶</div>
+                        <div className={styles.text}>۵شنبه</div>
+                    </div>
+                    <div className={styles.day}>
+                        <div className={styles.number}>۱۷</div>
+                        <div className={styles.text}>جمعه</div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className={styles.content}>
+
+                <div className={styles.main}>
+                    <div className={styles.section}>
+                            <div className={styles.event}>
+                                <div className={styles.topCard}>
+                                    <div>رخداد صرع</div>
+                                    <Share />
+                                </div>
+                            </div>
+
+                        <div className={styles.reports}>
+                            <div className={styles.topCard}>
+                                <div>گزارشات</div>
+                                <Note/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.reminder}>
+                        <div>یادآور دارو</div>
+                    </div>
+
+                    <div className={styles.knowledge}>
+                        <div>دانش روز</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 };
