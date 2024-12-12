28a53a34ef50e5432ab4ec7223ab192646ee1793
amirasahraee <87465114+amirasahraee@users.noreply.github.com>
2024-12-12 12:44:36 +0330

    #1 implement Login UI and components

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -1,7 +1,32 @@
+import styles from './Login.module.scss';
+import LogoArt from '@/assets/image/logoArt.png';
+import LogoText from '@/assets/image/logoText.png';
+import {Link} from "react-router-dom";
+
+
 export const Login = () => {
-  return (
-    <div>
-login
-    </div>
-  );
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.header}>
+                <img src={LogoArt} alt='' className={styles.logo}/>
+                <img src={LogoText} alt='' className={styles.logo}/>
+                <div className={styles.shadow} />
+
+            </div>
+            <div className={styles.main}>
+                <div className={styles.titleBox}>
+                    <div className={styles.title}>به صرع نگار خوش آمدید</div>
+                    <div className={styles.description}>دستیار دلسوز بیماران صرع و پزشکان</div>
+                </div>
+                <div className={styles.mobileBox}>
+                    <div className={styles.label}>شماره همراه خود را وارد نمایید</div>
+                    <input className={styles.input} type="text" placeholder='شماره موبایل'/>
+                    <Link to='/activation' className={styles.btnBox}>ارسال کد فعالسازی</Link>
+
+                </div>
+
+
+            </div>
+        </div>
+    );
 };
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -8,6 +8,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // components
 import { Home } from "@/app/Home/Home";
 import { Login } from "@/app/Login/Login.tsx";
+import {Activation} from "@/app/Login/components/Activation/Activation.tsx";
+
+
 import { AddModal } from "@/app/Home/_components/Modals/AddModal";
 import { EditModal } from "@/app/Home/_components/Modals/EditDrawer";
 import { DeleteModal } from "@/app/Home/_components/Modals/DeleteModal";
@@ -16,21 +19,20 @@ import { PrimaryLayout } from "@/layouts/PrimaryLayout/PrimaryLayout.tsx";
 //helpers
 import { RedirectAfterLogin } from "./components/RedirectAfterLogin/RedirectAfterLogin.tsx";
 
+
 const AppRouter = () => {
   const isLogin = useAppSelector((state) => state.auth.login.status === "success");
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" Component={AppRouterUtils.selectElement(true, PrimaryLayout)}>
-          <Route path={routes.home.href} Component={Home}>
-            <Route path={routes.home.modals.add.href} Component={AddModal} />
-            <Route path={routes.home.modals.edit.href()} Component={EditModal} />
-            <Route path={routes.home.modals.delete.href()} Component={DeleteModal} />
-          </Route>
+        <Route path="" Component={AppRouterUtils.selectElement(true, Home)}>
         </Route>
 
         <Route path={routes.login.href} Component={AppRouterUtils.selectElement(!isLogin, Login, RedirectAfterLogin)} />
+
+        <Route path='login' Component={Login} />
+        <Route path='activation' Component={Activation} />
       </Routes>
     </BrowserRouter>
   );
