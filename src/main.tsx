8e92b709eea0593bb3abe676e4bce591276a8210
AmirShafikhani <amirshafikhani.official@gmail.com>
2025-04-03 13:53:49 +0330

    refactor(styles): 🔧 global styles updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { Provider } from "react-redux";
 import { appStore } from "@/store/store";
 import AppRouter from "@/routes/AppRouter";
 import { createRoot } from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={appStore}>
     <AppRouter />
+    <ToastContainer closeOnClick rtl />
   </Provider>,
 );
