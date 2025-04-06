60d257d6da6e395d9db832ddb04d882d93e178a8
AmirShafikhani <amirshafikhani.official@gmail.com>
2025-04-06 19:39:57 +0330

    refactor(common): 🔧 toggle component  updated

--- src/common/Toggle/Toggle.tsx
+++ src/common/Toggle/Toggle.tsx
@@ -18,25 +18,30 @@ export const Toggle = <T extends FieldValues, Y = string>({
       name={name}
       control={control}
       rules={{ ...rules, validate: validate?.validate }}
-      render={({ field, fieldState }) => (
-        <div className={styles.container} data-error={!!fieldState.error}>
-          <input {...field} className={styles.input} />
-          {label && <label className={styles.label}>{label}</label>}
-          <div className={styles.toggle}>
-            {options.map((option) => (
-              <div
-                key={option.label}
-                className={styles.option}
-                data-active={option.value === field.value?.value}
-                onClick={() => onChangeHandler(option, field.onChange)}
-              >
-                {option.label}
-              </div>
-            ))}
+      render={({ field, fieldState }) => {
+        const _index = options.findIndex((item) => item.value === field.value?.value);
+        return (
+          <div className={styles.container} data-error={!!fieldState.error}>
+            <input {...field} className={styles.input} />
+            {label && <label className={styles.label}>{label}</label>}
+            <div className={styles.toggle}>
+              {options.map((option, index) => (
+                <div
+                  data-index={index}
+                  key={option.label}
+                  className={styles.option}
+                  data-active={_index === index}
+                  onClick={() => onChangeHandler(option, field.onChange)}
+                >
+                  {option.label}
+                </div>
+              ))}
+              {_index >= 0 && <div className={styles.activator} data-direction={_index === 0 ? "start" : "end"}></div>}
+            </div>
+            {fieldState.error && <span className={styles.error}>{fieldState.error?.message}</span>}
           </div>
-          {fieldState.error && <span className={styles.error}>{fieldState.error?.message}</span>}
-        </div>
-      )}
+        );
+      }}
     />
   );
 };
