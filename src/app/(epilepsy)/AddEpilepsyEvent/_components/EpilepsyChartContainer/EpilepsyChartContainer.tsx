7798e22f191387f28ffb1dde2894b4119bef84ca
AmirShafikhani <amirshafikhani.official@gmail.com>
2025-04-08 23:05:15 +0330

    refactor(app): 🔧 epilepsy extra components removed

--- src/app/(epilepsy)/AddEpilepsyEvent/_components/EpilepsyChartContainer/EpilepsyChartContainer.tsx
+++
@@ -1,8 +0,0 @@
-import { EpilepsyChart } from "@/common/EpilepsyChart/EpilepsyChart";
-import { useEpilepsyChartContainer } from "./useEpilepsyChartContainer";
-
-export const EpilepsyChartContainer = () => {
-  const { value } = useEpilepsyChartContainer();
-
-  return <EpilepsyChart size="lg" theme="purple" value={value} />;
-};
--- src/app/(epilepsy)/EditEpilepsyEvent/_components/EpilepsyChartContainer/useEpilepsyChartContainer.ts
+++
@@ -1,9 +0,0 @@
-import { useFormContext } from "react-hook-form";
-import { TEpilepsyEventForm } from "@/store/epilepsy/epilepsySlice.types";
-
-export const useEpilepsyChartContainer = () => {
-  const { watch } = useFormContext<TEpilepsyEventForm>();
-  const { value } = watch("severity");
-
-  return { value };
-};
