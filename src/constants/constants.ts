3fa9a1ed96d4205a8d89fa0e3d68922f2c39b373
AmirShafikhani <amirshafikhani.official@gmail.com>
2025-04-11 13:36:15 +0330

    refactor(configs): 🔧 app version added

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,5 @@
 export const TRANSITION_TIME = 200;
 
 export const PORTAL_ID = "portal";
+
+export const VERSION_ID = "0.0.0";
--- src/routes/components/RouterStateManager/RouterStateManager.tsx
+++ src/routes/components/RouterStateManager/RouterStateManager.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { VERSION_ID } from "@/constants/constants";
 import { RouterService } from "@/services/RouterService.ts";
 import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
 
@@ -5,5 +7,10 @@ export const RouterStateManager = () => {
   RouterService.navigate = useNavigate();
   RouterService.location = useLocation();
   RouterService.params = useParams<Record<string, string>>();
+
+  useEffect(() => {
+    if (import.meta.env.MODE === "production") console.log(`%cAPP VERSION => ${VERSION_ID}`, "color: #df6a6a;");
+  }, []);
+
   return <Outlet />;
 };
