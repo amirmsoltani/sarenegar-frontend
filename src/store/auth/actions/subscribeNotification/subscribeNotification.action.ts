c98e6b5bde48b8057deacb47b8f11c1e3916d655
amirsoltanik <amirsoltanik78@gmail.com>
2025-04-15 20:04:40 +0330

    fix notification subscription

--- src/store/auth/actions/subscribeNotification/subscribeNotification.action.ts
+++ src/store/auth/actions/subscribeNotification/subscribeNotification.action.ts
@@ -1,23 +1,19 @@
 import { StoreUtils } from "@/store/Store.utils.ts";
 
 import { NotificationHelper } from "@/helper/notification.ts";
-// import { apiNotificationNotificationRegisterCreate } from "@/services/api.ts";
+import { apiNotificationNotificationRegisterCreate } from "@/services/api.ts";
 
 export const subscribeNotificationAction = StoreUtils.createAsyncThunk("auth/subscribeNotification", async () => {
   if (await NotificationHelper.requestPermission()) {
     const subscription = await NotificationHelper.subscribeUser();
     if (subscription) {
-      const p256dh = subscription.getKey("p256dh");
-      const auth = subscription.getKey("auth");
-      if (p256dh && auth) {
-        // const decoder = new TextDecoder("utf-8");
-        console.log(subscription.endpoint, subscription.toJSON(), "******************");
-
-        // await apiNotificationNotificationRegisterCreate({
-        //   endpoint: subscription.endpoint,
-        //   p256dh: p256dh,
-        //   auth: decoder.decode(auth),
-        // });
+      const subscribeJson = subscription.toJSON();
+      if (subscribeJson.keys) {
+        await apiNotificationNotificationRegisterCreate({
+          endpoint: subscribeJson.endpoint!,
+          p256dh: (subscribeJson.keys as Record<string, string>).p256dh,
+          auth: (subscribeJson.keys as Record<string, string>).auth,
+        });
       }
     }
   }
