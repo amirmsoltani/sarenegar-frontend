fff0d213a9918a3e75939e63d1e6dca7ffd25ed4
amirsoltanik <amirsoltanik78@gmail.com>
2025-07-12 01:09:04 +0330

    fix production

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,55 @@
 import { resolve } from "node:path";
 import svgr from "vite-plugin-svgr";
+import { parse } from "dotenv";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
+import fs from "node:fs";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      include: "**/*.svg",
-      svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
-    }),
-    VitePWA({
-      srcDir: "src",
-      filename: "sw.js",
-      strategies: "injectManifest",
-      manifest: {
-        name: "اپیکال",
-        short_name: "اپیکال",
-        theme_color: "#1d2742",
-        description: "Recording seizure events to present to the doctor",
-        icons: [
-          { src: "icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "icon-512.png", sizes: "512x512", type: "image/png" },
-        ],
-      },
-      devOptions: { enabled: true, type: "module", navigateFallback: "index.html" },
-    }),
-  ],
-  resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        style: "compressed",
-        api: "modern-compiler",
-        additionalData: `@use "@/styles/mixins.scss" as *;`,
+export default () => {
+  let stack = "";
+  try {
+    stack = fs.readFileSync("stack.env", "utf8");
+  } catch (err) {
+    console.log(err);
+  }
+  process.env = { ...process.env, ...parse(stack) };
+
+  return defineConfig({
+    plugins: [
+      react(),
+      svgr({
+        include: "**/*.svg",
+        svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
+      }),
+      VitePWA({
+        srcDir: "src",
+        filename: "sw.js",
+        strategies: "injectManifest",
+        manifest: {
+          name: "اپیکال",
+          short_name: "اپیکال",
+          theme_color: "#1d2742",
+          description: "Recording seizure events to present to the doctor",
+          icons: [
+            { src: "icon-192.png", sizes: "192x192", type: "image/png" },
+            { src: "icon-512.png", sizes: "512x512", type: "image/png" },
+          ],
+        },
+        devOptions: { enabled: true, type: "module", navigateFallback: "index.html" },
+      }),
+    ],
+    resolve: {
+      alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          style: "compressed",
+          api: "modern-compiler",
+          additionalData: `@use "@/styles/mixins.scss" as *;`,
+        },
       },
     },
-  },
-});
+  });
+};
